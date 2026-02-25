39daee4c99f34e79d9f8b67ae82a4dbdecf94a5f
sseusti <stepan.s7ep@gmail.com>
2026-02-26 02:55:55 +0300

    fix(api): handle graceful shutdown and ListenAndServe errors correctly

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,23 +26,23 @@ func main() {
 		err := srv.ListenAndServe()
 		if err != nil {
 			if errors.Is(err, http.ErrServerClosed) {
-				log.Fatal(err)
+				return
 			}
-			return
-		}
-
-		sigCh := make(chan os.Signal, 1)
-		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-		<-sigCh
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		err = srv.Shutdown(ctx)
-		if err != nil {
 			log.Fatal(err)
 		}
-
-		return
 	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
 }
